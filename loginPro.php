25022a3f51009196ea92141922e1c107cc7cd101
Lawliet <haoyu.wong@foxmail.com>
2016-12-12 21:28:26 +0800

    登录功能bug修复,登陆注册页面样式美化

--- loginPro.php
+++ loginPro.php
@@ -1,8 +1,5 @@
-  <?php
-																									// 获取用户帐号密码信息
-    $name = $_POST['uname'];
+  <?php																						
     $passwd = $_POST['pwd'];	
-        																						// 链接数据库
     $servername = "localhost";
     $username = "root";
     $password = "root";
@@ -12,7 +9,7 @@
         //PDO链接
   			$conn->query('SET NAMES utf8'); 			//设置字符编码
         session_start(); 											//session开始
-        $sql = "select userid from User where username = '".$name."' and password = '".$passwd."'";
+        $sql = "select userid from User where username = '".$name."' AND password = '".$passwd."'";
         $res = $conn->query($sql); 					 //执行sql语句
         $rs = $res->fetch();									//获取查询结果
         if ($rs) {
