b52ba683f935a1c680c079359224b5246c19589f
Lawliet <haoyu.wong@foxmail.com>
2016-12-13 21:23:10 +0800

    session存储用户信息进入新的diss讨论版界面

---
+++ diss.php
@@ -0,0 +1,59 @@
+<!DOCTYPE html>
+<html lang="zh-cn">
+  <head>
+    <meta charset="utf-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <title>咔方网 - ( ゜- ゜)つロ  乾杯~  - CArFun!</title>
+    <link rel="stylesheet" type="text/css" href="css/bootstrap.min.css">
+    <link rel="stylesheet" type="text/css" href="css/carousel.css">
+    <link rel="shortcut icon" type="image/x-icon" href="img/car.ico" /> 
+    <style type="text/css">
+      .uname a{
+        font-weight: 10px;
+        font-size: 20px !important;
+        color: #1abc9c !important;
+      }
+    </style>
+  </head>
+  <body>
+    <!--    导航条    -->  
+    <div class="navbar-wrapper">
+        <div class="container">
+            <nav class="navbar navbar-inverse navbar-static-top" role="navigation">
+            <div class="container">
+                <div class="navbar-header">
+                <button type="button" class="navbar-toggle collapsed" data-toggle="collapse" data-target="#navbar" aria-expanded="false" aria-controls="navbar">
+                </button>
+                <a class="navbar-brand" href="index.html">咔方</a>
+              </div>
+              <div id="navbar" class="navbar-collapse collapse">
+                <ul class="nav navbar-nav">
+                  <li class="active"><a href="#">主页</a></li>
+                  <li><a href="regist.html">注册</a></li>
+                  <li><a href="#about">玩家讨论区</a></li>
+                  <li class="dropdown">
+                    <a href="#" class="dropdown-toggle" data-toggle="dropdown">更多 <span class="caret"></span></a>
+                    <ul class="dropdown-menu" role="menu">
+                      <li><a href="#">联系我们</a></li>
+                      <li><a href="#">线下活动</a></li>
+                    </ul>
+                   </li>
+                   <?php
+                    session_start();
+                    echo '<li class="uname"><a href="#">欢迎您：'.$_SESSION['username'].'</a></li>';
+                  ?>
+                 </ul>
+               </div>
+             </div>
+           </nav>
+         </div>
+        </div>
+    <script src="js/jquery-3.0.0.min.js"></script>
+    <script src="js/bootstrap.min.js"></script>
+  </body>
+</html>  
+
+
+
+  
--- loginPro.php
+++ loginPro.php
@@ -1,27 +1,61 @@
-  <?php																						
-    $passwd = $_POST['pwd'];	
+<!DOCTYPE html>
+<html lang="zh-cn">
+  <head>
+    <meta charset="utf-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <title>咔方网 - ( ゜- ゜)つロ  乾杯~  - CArFun!</title>
+    <link rel="stylesheet" type="text/css" href="css/bootstrap.min.css">
+    <link rel="stylesheet" type="text/css" href="css/carousel.css">
+    <link rel="shortcut icon" type="image/x-icon" href="img/car.ico" />
+    <style type="text/css">
+      body{
+        background-color: #95a5a6;
+      }
+      .box{
+        position: relative;
+        top: 200px;
+        margin: 0 auto;
+        width: 400px !important;
+      }
+    </style> 
+  </head>
+  <body>
+    <?php           
+    $name = $_POST['uname'];                                    
+    $passwd = $_POST['pwd'];    
     $servername = "localhost";
     $username = "root";
     $password = "root";
     $dbname = "cfDB";
     try {
-   		  $conn = new PDO("mysql:host=$servername;dbname=$dbname", $username, $password);
+          $conn = new PDO("mysql:host=$servername;dbname=$dbname", $username, $password);
         //PDO链接
-  			$conn->query('SET NAMES utf8'); 			//设置字符编码
-        session_start(); 											//session开始
+            $conn->query('SET NAMES utf8');             //设置字符编码
+        session_start();                                            //session开始
         $sql = "select userid from User where username = '".$name."' AND password = '".$passwd."'";
-        $res = $conn->query($sql); 					 //执行sql语句
-        $rs = $res->fetch();									//获取查询结果
+        $res = $conn->query($sql);                   //执行sql语句
+        $rs = $res->fetch();                                    //获取查询结果
         if ($rs) {
-        	echo "恭喜你进入咔方系统";
+            $_SESSION['username'] = $name;
+            echo $_SESSION['username']."恭喜你进入咔方系统";
+            echo '<script>window.location.href = "diss.php"</script>';
         }
         else{
-        	echo "登录信息有误";
+            echo '<div class="alert alert-danger box">
+                  <a href="login.html" class="alert-link">用户信息输入错误，点击返回。</a>
+                  </div>';
         }
-   	}
-	  catch(PDOException $e){
-		      echo $e->getMessage();
-		}
+    }
+    catch(PDOException $e){
+       echo $e->getMessage();
+    }
+    ?>
+    <script src="js/jquery-3.0.0.min.js"></script>
+    <script src="js/bootstrap.min.js"></script>
+  </body>
+</html>  
 
 
-?>
+
+  
